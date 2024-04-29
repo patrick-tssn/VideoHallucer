f9c38ca2e0c24eff917aea031edf6ad4686bc725
patrick-tssn <flagwyx@gmail.com>
2024-04-29 10:47:01 +0800

    update

--- evaluations/evaluation.py
+++ evaluations/evaluation.py
@@ -23,10 +23,34 @@ CKPT_DIR = configs['CKPT_DIR']
 
 
 def load_model(TESTING_MODEL):
-    if TESTING_MODEL == "VideoLLaVA":
+    if TESTING_MODEL == 'VideoChatGPT':
+        from videochatgpt_modeling import VideoChatGPT
+        ckpt_path = f"{CKPT_DIR}/Video-ChatGPT-7B"
+        model = VideoChatGPT({"model_path": ckpt_path, "device": 0})
+    elif TESTING_MODEL == "Valley":
+        from valley_modeling import Valley
+        ckpt_path = f"{CKPT_DIR}/Valley2-7b"
+        model = Valley({"model_path": ckpt_path, "device": 0})
+    elif TESTING_MODEL == "Video-LLaMA-2":
+        from videollama_modeling import VideoLLaMA
+        ckpt_path = f"{CKPT_DIR}/Video-LLaMA-2-7B-Finetuned"
+        model = VideoLLaMA({"model_path": ckpt_path, "device": 0})
+    elif TESTING_MODEL == "VideoChat2":
+        from videochat_modeling import VideoChat
+        ckpt_path = f"{CKPT_DIR}/VideoChat2"
+        model = VideoChat({"model_path": ckpt_path, "device": 0})
+    elif TESTING_MODEL == "VideoLLaVA":
         from videollava_modeling import VideoLLaVA
-        ckpt_path = f'{CKPT_DIR}/Video-LLaVA-7B'
+        ckpt_path = f"{CKPT_DIR}/Video-LLaVA-7B"
         model = VideoLLaVA({"model_path": ckpt_path, "device": 0})
+    elif TESTING_MODEL == "LLaMA-VID":
+        from llamavid_modeling import LLaMAVID
+        ckpt_path = f"{CKPT_DIR}/LLaMA-VID-7B"
+        model = LLaMAVID({"model_path": ckpt_path, "device": 0})
+    elif TESTING_MODEL == "VideoLaVIT":
+        from videolavit_modeling import VideoLaVIT
+        ckpt_path = f"{CKPT_DIR}/Video-LaVIT-v1"
+        model = VideoLaVIT({"model_path": ckpt_path, "device": 0})
     elif TESTING_MODEL == "Gemini-1.5-pro":
         from gemini_modeling import Gemini
         model = Gemini({"model_path": None, "device": 0})
@@ -39,7 +63,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_name", type=str,
                         default="", 
-                        choices=["VideoLLaVA", "Gemini-1.5-pro"])
+                        choices=["VideoChatGPT", "Valley", "Video-LLaMA-2", "VideoChat2", "VideoLLaVA", "LLaMA-VID", "VideoLaVIT", "Gemini-1.5-pro"])
 
     parser.add_argument(
         "--output_dir_path", type=str, default="results",
